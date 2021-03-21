0b91838625581d312532a71a90c39718ab7f3949
mahir31 <mahirajmal@live.com>
2021-03-21 22:02:39 +0000

    added milestones

--- film.py
+++ film.py
@@ -7,12 +7,25 @@ while True:
             for number, z in enumerate(y, 1):
                 print(f'{number}. {z}')
 
-    if x == 'add':
-        print('input movie name')
+    elif x == 'add':
+        print('input title')
         x = input('> ')
         with open("film.txt", "a") as y:
             y.write(f'\n{x}')
         print(f'movie {x} has been added!')
-    
-    if x == 'exit':
+
+    elif x == 'milestone':
+        print('input number')
+        x = input('> ')
+        with open("film.txt", "r") as y:
+            y = [y.strip('\n') for y in (y.readlines())]
+            print(y[int(x)])
+
+    elif x == 'help':
+        print('commands:\nadd - add a movie to the list\nprint - display movie list\nhelp - display command list\nexit - exit current runtime')
+
+    elif x == 'exit':
         break
+
+    else:
+        print('invalid command! type "help" for command list')
